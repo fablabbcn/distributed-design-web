ebcf418bb342ac93863f5d8700e421fb50af17b7
Nestor Vera <nestorvera@me.com>
2019-03-16 18:31:39 +0100

    Fix rendering empty blocks in Talent posts (CSV import)

--- single.php
+++ single.php
@@ -36,57 +36,66 @@ get_header(); ?>
 	<?php while ( the_flexible_field( 'post_content' ) ) : ?>
 
 		<?php if ( 'text_content' === get_row_layout() ) : ?>
+			<?php if ( get_sub_field( 'text' ) ) : ?>
 
-			<div class="cf bootstrap-wrapper post-content">
-				<div class="row row-eq-height">
-					<div class="fl single_first-column">&nbsp;</div>
-					<div class="fl single_second-column content-column"><?php the_sub_field( 'text' ); ?></div>
-					<div class="fl single_third-column">&nbsp;</div>
+				<div class="cf bootstrap-wrapper post-content">
+					<div class="row row-eq-height">
+						<div class="fl single_first-column">&nbsp;</div>
+						<div class="fl single_second-column content-column"><?php the_sub_field( 'text' ); ?></div>
+						<div class="fl single_third-column">&nbsp;</div>
+					</div>
 				</div>
-			</div>
+
+			<?php endif; ?>
 
 
 		<?php elseif ( 'image_content' === get_row_layout() ) : ?>
+			<?php if ( get_sub_field( 'heading' ) || get_sub_field( 'sub_heading' ) || get_sub_field( 'bottom' ) || get_sub_field( 'image' ) ) : ?>
 
-			<div class="cf bootstrap-wrapper post-content">
-				<div class="row row-eq-height">
-					<div class="fl single_first-column">
-						<p class="b"><?php the_sub_field( 'heading' ); ?></p>
-						<p>-</p>
-						<p><?php the_sub_field( 'sub_heading' ); ?></p>
-						<p class="absolute bottom-2 b"><?php the_sub_field( 'bottom' ); ?></p>
+				<div class="cf bootstrap-wrapper post-content">
+					<div class="row row-eq-height">
+						<div class="fl single_first-column">
+							<p class="b"><?php the_sub_field( 'heading' ); ?></p>
+							<p>-</p>
+							<p><?php the_sub_field( 'sub_heading' ); ?></p>
+							<p class="absolute bottom-2 b"><?php the_sub_field( 'bottom' ); ?></p>
+						</div>
+						<div class="fl single_second-column bg-center bg-cover" style="background: url('<?php the_sub_field( 'image' ); ?>');">
+							<?php the_post_thumbnail(); ?>
+						</div>
+						<div class="fl single_third-column">&nbsp;</div>
 					</div>
-					<div class="fl single_second-column" style="background: url('<?php the_sub_field( 'image' ); ?>') center no-repeat; background-size: cover;">
-						<?php the_post_thumbnail(); ?>
-					</div>
-					<div class="fl single_third-column">&nbsp;</div>
 				</div>
-			</div>
+
+			<?php endif; ?>
 
 
 		<?php elseif ( 'slider_content' === get_row_layout() ) : ?>
+			<?php if ( get_sub_field( 'heading' ) || get_sub_field( 'sub_heading' ) || get_sub_field( 'bottom' ) || get_sub_field( 'image' ) ) : ?>
 
-			<div class="cf bootstrap-wrapper post-content">
-				<div class="row row-eq-height">
-					<div class="fl single_first-column">
-						<p class="b"><?php the_sub_field( 'heading' ); ?></p>
-						<p>-</p>
-						<p><?php the_sub_field( 'sub_heading' ); ?></p>
-						<p class="absolute bottom-2 b"><?php the_sub_field( 'bottom' ); ?></p>
-					</div>
-					<div class="fl single_slider-column">
-						<?php $images = get_sub_field( 'image' ); ?>
-					<?php if ( $images ) : ?>
-						<div class="post-slider">
-						<?php foreach ( $images as $image ) : ?>
-							<div><img src="<?php echo esc_attr( $image['url'] ); ?>"/></div>
-						<?php endforeach; ?>
+				<div class="cf bootstrap-wrapper post-content">
+					<div class="row row-eq-height">
+						<div class="fl single_first-column">
+							<p class="b"><?php the_sub_field( 'heading' ); ?></p>
+							<p>-</p>
+							<p><?php the_sub_field( 'sub_heading' ); ?></p>
+							<p class="absolute bottom-2 b"><?php the_sub_field( 'bottom' ); ?></p>
 						</div>
-					<?php endif; ?>
+						<div class="fl single_slider-column">
+							<?php $images = get_sub_field( 'image' ); ?>
+						<?php if ( $images ) : ?>
+							<div class="post-slider">
+							<?php foreach ( $images as $image ) : ?>
+								<div><img src="<?php echo esc_attr( $image['url'] ); ?>"/></div>
+							<?php endforeach; ?>
+							</div>
+						<?php endif; ?>
+						</div>
+						<div class="fl single_third-column">&nbsp;</div>
 					</div>
-					<div class="fl single_third-column">&nbsp;</div>
 				</div>
-			</div>
+
+			<?php endif; ?>
 
 
 		<?php endif; ?>
