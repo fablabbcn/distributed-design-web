602fa0b878a1da5e35d9525cb89d91b35390d3d2
Nestor Vera <nestorvera@me.com>
2019-03-20 13:03:23 +0100

    Fix `focus` styles on Talent Archive

--- template-parts/post/content-talent.php
+++ template-parts/post/content-talent.php
@@ -9,7 +9,7 @@ $project = get_field( 'project' );
 $mod     = ( $wp_query->current_post ) % 4;
 $is_even = 0 === $mod || 1 === $mod;
 
-$a_classes  = implode( ' ', array( 'flex', $is_even ? 'flex-row' : 'flex-row-reverse', 'w-full hover:text-inherit no-underline' ) );
+$a_classes  = implode( ' ', array( 'flex', $is_even ? 'flex-row' : 'flex-row-reverse', 'w-full hocus:text-inherit no-underline' ) );
 $dl_classes = 'flex flex-col flex-grow justify-center p-10 md:p-15 border-px';
 $dt_classes = 'leading-normal font-light capitalize';
 $dd_classes = 'leading-normal font-semibold';
@@ -17,14 +17,14 @@ $dd_classes = 'leading-normal font-semibold';
 ?>
 
 
-<div id="post-<?php the_ID(); ?>" class="group flex w-full">
-	<a class="<?php echo esc_attr( $a_classes ); ?>" href="<?php the_permalink(); ?>">
+<div id="post-<?php the_ID(); ?>" class="flex w-full">
+	<a class="group <?php echo esc_attr( $a_classes ); ?>" href="<?php the_permalink(); ?>">
 
 		<div class="relative flex flex-col w-full bg-black">
-			<?php the_post_thumbnail( 'post-list-thumbnails-square', array( 'class' => 'block w-full h-full group-hover:opacity-70 border-px object-cover' ) ); ?>
+			<?php the_post_thumbnail( 'post-list-thumbnails-square', array( 'class' => 'block w-full h-full group-hocus:opacity-70 border-px object-cover' ) ); ?>
 		</div>
 
-		<div class="flex flex-col w-full group-hover:bg-yellow">
+		<div class="flex flex-col w-full group-hocus:bg-yellow">
 
 			<dl class="<?php echo esc_attr( $dl_classes ); ?>">
 				<dt class="<?php echo esc_attr( $dt_classes ); ?>"><?php echo esc_html( $winer['title'] ); ?></dt>
