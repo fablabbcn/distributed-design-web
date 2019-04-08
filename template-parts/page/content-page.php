9331c500f16b945897139bbc76718a1350401528
Nestor Vera <nestorvera@me.com>
2019-04-08 12:38:14 +0200

    Fix page header

--- template-parts/page/content-page.php
+++ template-parts/page/content-page.php
@@ -2,6 +2,14 @@
 /**
  * Template part for displaying page content in page.php
  */
+
+$cp_classes = array(
+	'columns' => array(
+		array( 'w-full lg:w-1/4 p-20 lg:px-40 border-t' ),
+		array( 'w-full lg:w-3/4 p-20 lg:px-40 border-t lg:border-l' ),
+	),
+);
+
 ?>
 
 <main class="flex-grow">
@@ -11,9 +19,11 @@
 		<?php set_query_var( 'title', get_the_title() ); ?>
 		<?php get_template_part( 'template-parts/blocks/header' ); ?>
 
-		<section class="flex border-b">
-			<div class="w-full lg:w-1/4 mr-auto p-20"><?php get_template_part( 'template-parts/singular/side' ); ?></div>
-			<div class="w-full lg:w-3/4 ml-auto lg:border-l"><?php get_template_part( 'template-parts/singular/hero' ); ?></div>
+		<section class="post-content rich-text">
+			<div class="flex flex-wrap lg:flex-no-wrap border-b">
+				<div class="<?php the_classes( $cp_classes['columns'][0] ); ?>"><?php get_template_part( 'template-parts/singular/side' ); ?></div>
+				<div class="<?php the_classes( $cp_classes['columns'][1] ); ?>"><?php get_template_part( 'template-parts/singular/hero' ); ?></div>
+			</div>
 		</section>
 
 		<section class="rich-text">
