45883ba2668339722897b5e59a2e91ad632dcc71
Nestor Vera <nestorvera@me.com>
2019-07-10 13:00:09 +0200

    Mobile: Fix heading font-sizes in Single layouts

--- template-parts/blocks/header.php
+++ template-parts/blocks/header.php
@@ -1,5 +1,5 @@
 <header class="w-full font-oswald uppercase border-b">
-	<h1 class="lg:w-3/4 lg:ml-auto p-20 lg:px-40 text-16 md:text-20 lg:text-41 leading-normal lg:border-l">
+	<h1 class="lg:w-3/4 lg:ml-auto p-20 lg:px-40 text-16 lg:text-41 leading-normal lg:border-l">
 		<?php echo esc_html( $title ); ?>
 	</h1>
 </header>
