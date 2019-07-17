d370f0c897eeee9cbb498e6585d3a5ba26760f55
Nestor Vera <nestorvera@me.com>
2019-07-17 16:32:57 +0200

    Hide empty cells on small screens

--- single.php
+++ single.php
@@ -11,11 +11,13 @@ $post = $_post ?: $post;
 $s_classes = array(
 	'section' => array( 'post-content rich-text relative' ),
 	'layout'  => array( 'flex flex-wrap lg:flex-no-wrap' ),
-	'columns' => array(
-		array( 'flex flex-col w-full lg:w-1/4 p-20 lg:px-40 border-t' ),
-		array( 'flex flex-col w-full lg:w-3/4 p-20 lg:px-40 border-t lg:border-l' ),
-		array( 'hidden md:flex flex-col w-full lg:w-auto p-20 lg:px-40 border-t lg:border-l' ),
-	),
+	'columns' => function ( $has_details ) {
+		return array(
+			array( $has_details ? 'flex' : 'hidden', 'lg:flex flex-col w-full lg:w-1/4 p-20 lg:px-40 border-t' ),
+			array( 'flex lg:flex flex-col w-full lg:w-3/4 p-20 lg:px-40 border-t lg:border-l' ),
+			array( 'hidden lg:flex flex-col w-full lg:w-auto p-20 lg:px-40 border-t lg:border-l' ),
+		);
+	},
 );
 
 ?>
@@ -32,9 +34,9 @@ $s_classes = array(
 
 		<section class="<?php the_classes( $s_classes['section'] ); ?>">
 			<div data-layout="hero" class="<?php the_classes( $s_classes['layout'] ); ?>">
-				<div class="<?php the_classes( $s_classes['columns'][0] ); ?>"><?php include locate_template( 'template-parts/singular/side.php' ); ?></div>
-				<div class="<?php the_classes( $s_classes['columns'][1] ); ?>"><?php include locate_template( 'template-parts/singular/hero.php' ); ?></div>
-				<div class="<?php the_classes( $s_classes['columns'][2] ); ?>"><?php include locate_template( 'template-parts/post/aside.php' ); ?></div>
+				<div class="<?php the_classes( $s_classes['columns'](true)[0] ); ?>"><?php include locate_template( 'template-parts/singular/side.php' ); ?></div>
+				<div class="<?php the_classes( $s_classes['columns'](true)[1] ); ?>"><?php include locate_template( 'template-parts/singular/hero.php' ); ?></div>
+				<div class="<?php the_classes( $s_classes['columns'](true)[2] ); ?>"><?php include locate_template( 'template-parts/post/aside.php' ); ?></div>
 			</div>
 		</section>
 
@@ -57,21 +59,21 @@ $s_classes = array(
 						<div data-layout="<?php echo esc_attr( get_row_layout() ); ?>" class="<?php the_classes( $s_classes['layout'] ); ?>">
 
 						<?php if ( 'text_content' === get_row_layout() ) : ?>
-							<div class="<?php the_classes( $s_classes['columns'][0] ); ?>"><?php include locate_template( 'template-parts/singular/details.php' ); ?></div>
-							<div class="<?php the_classes( $s_classes['columns'][1] ); ?>"><?php the_sub_field( 'text' ); ?></div>
-							<div class="<?php the_classes( $s_classes['columns'][2] ); ?>">&nbsp;</div>
+							<div class="<?php the_classes( $s_classes['columns']($details)[0] ); ?>"><?php include locate_template( 'template-parts/singular/details.php' ); ?></div>
+							<div class="<?php the_classes( $s_classes['columns']($details)[1] ); ?>"><?php the_sub_field( 'text' ); ?></div>
+							<div class="<?php the_classes( $s_classes['columns']($details)[2] ); ?>">&nbsp;</div>
 
 
 						<?php elseif ( 'image_content' === get_row_layout() ) : ?>
-							<div class="<?php the_classes( $s_classes['columns'][0] ); ?>"><?php include locate_template( 'template-parts/singular/details.php' ); ?></div>
-							<div class="<?php the_classes( $s_classes['columns'][1] ); ?>"><?php include locate_template( 'template-parts/singular/image.php' ); ?></div>
-							<div class="<?php the_classes( $s_classes['columns'][2] ); ?>">&nbsp;</div>
+							<div class="<?php the_classes( $s_classes['columns']($details)[0] ); ?>"><?php include locate_template( 'template-parts/singular/details.php' ); ?></div>
+							<div class="<?php the_classes( $s_classes['columns']($details)[1] ); ?>"><?php include locate_template( 'template-parts/singular/image.php' ); ?></div>
+							<div class="<?php the_classes( $s_classes['columns']($details)[2] ); ?>">&nbsp;</div>
 
 
 						<?php elseif ( 'slider_content' === get_row_layout() ) : ?>
-							<div class="<?php the_classes( $s_classes['columns'][0] ); ?>"><?php include locate_template( 'template-parts/singular/details.php' ); ?></div>
-							<div class="<?php the_classes( $s_classes['columns'][1] ); ?>"><?php include locate_template( 'template-parts/singular/slider.php' ); ?></div>
-							<div class="<?php the_classes( $s_classes['columns'][2] ); ?>">&nbsp;</div>
+							<div class="<?php the_classes( $s_classes['columns']($details)[0] ); ?>"><?php include locate_template( 'template-parts/singular/details.php' ); ?></div>
+							<div class="<?php the_classes( $s_classes['columns']($details)[1] ); ?>"><?php include locate_template( 'template-parts/singular/slider.php' ); ?></div>
+							<div class="<?php the_classes( $s_classes['columns']($details)[2] ); ?>">&nbsp;</div>
 
 
 						<?php endif; ?>
