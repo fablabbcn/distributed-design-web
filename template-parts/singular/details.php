aaa8c1828f267b8b316ad8c7f8e89b8748890245
Nestor Vera <nestorvera@me.com>
2019-08-01 14:24:09 +0200

    Fix bottom position when other fields are filled

--- template-parts/singular/details.php
+++ template-parts/singular/details.php
@@ -16,16 +16,16 @@ $d_classes = array(
 
 <?php if ( $not_empty ) : ?>
 	<?php if ( array_key_exists( 'heading', $details ) && $details['heading'] ) : ?>
-		<p class="<?php the_classes( $d_classes['heading'] ); ?> "><?php wp_kses_ddmp( $details['heading'] ); ?></p>
+		<p class="<?php the_classes( $d_classes['heading'] ); ?>"><?php wp_kses_ddmp( $details['heading'] ); ?></p>
 		<p>-</p>
 	<?php endif; ?>
 
 	<?php if ( array_key_exists( 'subheading', $details ) && $details['subheading'] ) : ?>
-		<p class="<?php the_classes( $d_classes['subheading'] ); ?> "><?php wp_kses_ddmp( $details['subheading'] ); ?></p>
+		<p class="<?php the_classes( $d_classes['subheading'] ); ?>"><?php wp_kses_ddmp( $details['subheading'] ); ?></p>
 	<?php endif; ?>
 
 	<?php if ( array_key_exists( 'bottom', $details ) && $details['bottom'] ) : ?>
-		<p class="<?php the_classes( $d_classes['bottom'] ); ?> "><?php wp_kses_ddmp( $details['bottom'] ); ?></p>
+		<p class="<?php the_classes( $d_classes['bottom'] ); ?>" style="margin-top: auto;"><?php wp_kses_ddmp( $details['bottom'] ); ?></p>
 	<?php endif; ?>
 
 
