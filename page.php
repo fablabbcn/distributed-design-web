bc37f84a2d331ab9606f7e8955cb988cc8ef58a6
Nestor Vera <nestorvera@me.com>
2020-05-07 14:05:27 +0200

    Show header with title in front-page

--- page.php
+++ page.php
@@ -5,15 +5,31 @@
 
 get_header();
 
-
-if ( have_posts() ) {
-	while ( have_posts() ) {
-		the_post();
-		get_template_part( 'template-parts/page/content', is_front_page() ? 'frontpage' : 'page' );
-	}
-} else {
-	get_template_part( 'template-parts/post/content', 'none' );
-}
+?>
 
 
-get_footer();
+<main class="flex-grow">
+
+	<article class="">
+		<?php set_query_var( 'title', get_the_title() ); ?>
+		<?php get_template_part( 'template-parts/blocks/header' ); ?>
+
+		<?php
+
+		if ( have_posts() ) {
+			while ( have_posts() ) {
+				the_post();
+				get_template_part( 'template-parts/page/content', is_front_page() ? 'frontpage' : 'page' );
+			}
+		} else {
+			get_template_part( 'template-parts/post/content', 'none' );
+		};
+
+		?>
+
+	</article>
+
+</main>
+
+
+<?php get_footer(); ?>
--- template-parts/page/content-page.php
+++ template-parts/page/content-page.php
@@ -12,24 +12,14 @@ $cp_classes = array(
 
 ?>
 
-<main class="flex-grow">
 
-	<article class="">
+<section class="post-content rich-text">
+	<div class="lg:flex-no-wrap flex flex-wrap border-b">
+		<div class="<?php the_classes( $cp_classes['columns'][0] ); ?>"><?php get_template_part( 'template-parts/singular/side' ); ?></div>
+		<div class="<?php the_classes( $cp_classes['columns'][1] ); ?>"><?php get_template_part( 'template-parts/singular/hero' ); ?></div>
+	</div>
+</section>
 
-		<?php set_query_var( 'title', get_the_title() ); ?>
-		<?php get_template_part( 'template-parts/blocks/header' ); ?>
-
-		<section class="post-content rich-text">
-			<div class="flex flex-wrap lg:flex-no-wrap border-b">
-				<div class="<?php the_classes( $cp_classes['columns'][0] ); ?>"><?php get_template_part( 'template-parts/singular/side' ); ?></div>
-				<div class="<?php the_classes( $cp_classes['columns'][1] ); ?>"><?php get_template_part( 'template-parts/singular/hero' ); ?></div>
-			</div>
-		</section>
-
-		<section class="rich-text">
-			<?php get_template_part( 'template-parts/blocks/acf-flexible-content/content' ); ?>
-		</section>
-
-	</article>
-
-</main>
+<section class="rich-text">
+	<?php get_template_part( 'template-parts/blocks/acf-flexible-content/content' ); ?>
+</section>
