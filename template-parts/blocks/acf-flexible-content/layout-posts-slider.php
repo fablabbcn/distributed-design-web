95d9e338841e4a3b5e1bb9798abd56bc972aae91
Nestor Vera <nestorvera@me.com>
2020-05-07 19:46:38 +0200

    Fix broken read more links

--- template-parts/blocks/acf-flexible-content/layout-posts-slider.php
+++ template-parts/blocks/acf-flexible-content/layout-posts-slider.php
@@ -25,7 +25,7 @@ $_items = get_sub_field( 'featured_posts' );
 						<p class="text-16"><?php echo get_post_type_object( $_item->post_type )->label; ?></p>
 						<p class="text-20 lg:text-36 font-oswald"><?php echo $_item->post_title; ?></p>
 						<p class="mt-20 text-14 lg:text-18">
-							<a class="inline-flex justify-center w-auto lg:w-full max-w-screen-xs items-center py-10 px-20 bg-white hocus:text-black hocus:bg-primary text-center no-underline border rounded-full overflow-hidden" href="<?php esc_url( get_permalink( $_item->ID ) ); ?>">Read More</a>
+							<a class="inline-flex justify-center w-auto lg:w-full max-w-screen-xs items-center py-10 px-20 bg-white hocus:text-black hocus:bg-primary text-center no-underline border rounded-full overflow-hidden" href="<?php echo esc_url( get_permalink( $_item->ID ) ); ?>">Read More</a>
 						</p>
 					</div>
 				</div>
