a88eec3b8fccf1a46c076d125d790630d3a0ba77
Nestor Vera <nestorvera@me.com>
2020-05-20 16:40:48 +0200

    Fix filters overflowing containers in small screens

--- template-parts/blocks/header.php
+++ template-parts/blocks/header.php
@@ -1,10 +1,10 @@
-<header class="w-full font-oswald uppercase border-b">
+<header class="w-full font-oswald uppercase border-b overflow-hidden">
 
 	<h1 class="lg:w-3/4 lg:ml-auto p-20 lg:px-40 text-16 lg:text-41 leading-normal lg:border-l">
 		<?php echo esc_html( $title ); ?>
 	</h1>
 
-	<div class="flex -mx-border">
+	<div class="flex -mx-border overflow-x-auto overflow-y-hidden">
 		<?php if ( 'talent' === get_post_type() ) : ?>
 			<?php // echo do_shortcode( '[facetwp facet="categories_talent"]' ); ?>
 			<?php echo do_shortcode( '[facetwp facet="categories_talent_fields"]' ); ?>
