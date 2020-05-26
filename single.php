70652fb0a811e13de552c7d42e71c813fb9627fb
Nestor Vera <nestorvera@me.com>
2020-05-26 17:54:22 +0200

    Let editors disable the border between columns and their parent block

--- single.php
+++ single.php
@@ -11,11 +11,12 @@ $post = isset( $_post ) ? $_post : $post;
 $s_classes = array(
 	'section' => array( 'post-content rich-text relative' ),
 	'layout'  => array( 'flex flex-wrap lg:flex-no-wrap' ),
-	'columns' => function ( $has_details ) {
+	'columns' => function ( $has_details, $has_border = true ) {
+		$base_classes = $has_border ? 'p-20 lg:p-40 border-t' : 'p-20 pt-0 lg:p-40 lg:pt-0';
 		return array(
-			array( $has_details ? 'flex' : 'hidden', 'lg:flex flex-col w-full lg:w-1/4 p-20 lg:p-40 border-t' ),
-			array( $has_details ? 'flex' : 'hidden', 'lg:flex flex-col w-full lg:w-3/4 p-20 lg:p-40 border-t lg:border-l' ),
-			array( 'hidden lg:flex flex-col w-full lg:w-auto p-20 lg:p-40 border-t lg:border-l' ),
+			array( $has_details ? 'flex' : 'hidden', 'lg:flex flex-col w-full', $base_classes, 'lg:w-1/4' ),
+			array( $has_details ? 'flex' : 'hidden', 'lg:flex flex-col w-full', $base_classes, 'lg:w-3/4 lg:border-l' ),
+			array( 'hidden lg:flex flex-col w-full lg:w-auto', $base_classes, 'lg:border-l' ),
 		);
 	},
 );
@@ -45,7 +46,8 @@ $s_classes = array(
 			<?php while ( have_rows( 'post_content' ) ) : ?>
 				<?php if ( count( array_filter( the_row() ) ) > 1 ) : ?>
 					<?php
-					$details = array_filter(
+					$has_border = 'multi_columns' === get_row_layout() ? get_sub_field( 'has_border' ) : true;
+					$details    = array_filter(
 						array(
 							'heading'    => get_sub_field( 'heading' ),
 							'subheading' => get_sub_field( 'sub_heading' ),
@@ -57,19 +59,19 @@ $s_classes = array(
 					<section class="<?php the_classes( $s_classes['section'] ); ?>">
 						<div data-layout="<?php echo esc_attr( get_row_layout() ); ?>" class="<?php the_classes( $s_classes['layout'] ); ?>">
 
-							<div class="<?php the_classes( $s_classes['columns']($details)[0] ); ?>"><?php include locate_template( 'template-parts/singular/details.php' ); ?></div>
+							<div class="<?php the_classes( $s_classes['columns']($details, $has_border)[0] ); ?>"><?php include locate_template( 'template-parts/singular/details.php' ); ?></div>
 
 							<?php if ( 'text_content' === get_row_layout() ) : ?>
-								<div class="<?php the_classes( $s_classes['columns'](true)[1] ); ?>"><?php the_sub_field( 'text' ); ?></div>
+								<div class="<?php the_classes( $s_classes['columns'](true, $has_border)[1] ); ?>"><?php the_sub_field( 'text' ); ?></div>
 							<?php elseif ( 'image_content' === get_row_layout() ) : ?>
-								<div class="<?php the_classes( $s_classes['columns'](true)[1] ); ?>"><?php include locate_template( 'template-parts/singular/image.php' ); ?></div>
+								<div class="<?php the_classes( $s_classes['columns'](true, $has_border)[1] ); ?>"><?php include locate_template( 'template-parts/singular/image.php' ); ?></div>
 							<?php elseif ( 'slider_content' === get_row_layout() ) : ?>
-								<div class="<?php the_classes( $s_classes['columns'](true)[1] ); ?>"><?php include locate_template( 'template-parts/singular/slider.php' ); ?></div>
+								<div class="<?php the_classes( $s_classes['columns'](true, $has_border)[1] ); ?>"><?php include locate_template( 'template-parts/singular/slider.php' ); ?></div>
 							<?php elseif ( 'multi_columns' === get_row_layout() ) : ?>
-								<div class="<?php the_classes( $s_classes['columns'](true)[1] ); ?>"><?php include locate_template( 'template-parts/singular/columns.php' ); ?></div>
+								<div class="<?php the_classes( $s_classes['columns'](true, $has_border)[1] ); ?>"><?php include locate_template( 'template-parts/singular/columns.php' ); ?></div>
 							<?php endif; ?>
 
-							<div class="<?php the_classes( $s_classes['columns'](false)[2] ); ?>">&nbsp;</div>
+							<div class="<?php the_classes( $s_classes['columns'](false, $has_border)[2] ); ?>">&nbsp;</div>
 
 						</div>
 					</section>
