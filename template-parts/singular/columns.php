475961535ba74953693e75fb4ebfc5191a015a49
Nestor Vera <nestorvera@me.com>
2020-05-26 17:21:21 +0200

    Improve column styles

--- template-parts/singular/columns.php
+++ template-parts/singular/columns.php
@@ -5,7 +5,7 @@ $_items = get_sub_field( 'items' );
 ?>
 
 
-<div class="flex flex-wrap items-baseline -m-10 lg:-m-20 flex-1">
+<div class="flex flex-wrap justify-center items-baseline -m-10 lg:-m-20 flex-1">
 	<?php foreach ( $_items as $_item ) : ?>
 
 		<div class="w-full sm:w-1/2 md:w-1/3 p-10 lg:p-20">
@@ -19,7 +19,7 @@ $_items = get_sub_field( 'items' );
 			<?php if ( $_item['image'] ) : ?>
 				<div class="w-full mb-20">
 					<figure class="">
-						<?php echo wp_get_attachment_image( $_item['image'], 'post-list-thumbnails-square', false, array( 'class' => 'block' ) ); ?>
+						<?php echo wp_get_attachment_image( $_item['image'], 'post-list-thumbnails-square', false, array( 'class' => 'block mx-auto' ) ); ?>
 					</figure>
 				</div>
 			<?php endif; ?>
