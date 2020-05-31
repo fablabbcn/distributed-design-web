cbc977cd12ce5ff1bec401ffde1a33249e7acb7f
Nestor Vera <nestorvera@me.com>
2020-05-31 20:45:29 +0200

    Add awards form specific styles

--- template-parts/blocks/modal-form.php
+++ template-parts/blocks/modal-form.php
@@ -15,14 +15,15 @@ $n_classes = array(
 <div
 	data-modal
 	id="<?php echo esc_attr( $modal_id ); ?>"
+	<?php // TODO: Check if form was submitted and show if so instead of hiding; ?>
 	class="clip z-50 fixed pin justify-center w-full h-full overflow-x-hidden overflow-y-auto"
 >
 	<button data-clip="<?php echo esc_attr( $modal_id ); ?>" class="fixed pin w-full h-full bg-black-50 cursor-pointer"></button>
 
-	<div data-modal="container" class="flex flex-col justify-center items-center w-full my-auto p-20 md:pt-160 md:px-45 md:pb-85">
+	<div data-modal="container" class="flex flex-col justify-center items-center w-full my-auto p-10 md:pt-160 md:px-45 md:pb-85">
 		<div class="z-50 relative w-full max-w-screen-md bg-gray" className="p-20 border">
 
-			<header class="relative flex flex-col w-full p-20 border border-b-0">
+			<header class="relative flex flex-col w-full p-10 border border-b-0">
 				<p class="<?php the_classes( $n_classes['title'] ); ?>"><?php wp_kses_ddmp( "APPLY TO THE DISTRIBUTED DESIGN AWARDS" ); ?></p>
 			</header>
 
