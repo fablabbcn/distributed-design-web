d9a4bfdf8b7bd0e3cb59ce1c60c01684983b2ca5
Nestor Vera <nestorvera@me.com>
2023-03-16 13:44:20 +0100

    Show latest posts sections only on front page

--- page.php
+++ page.php
@@ -121,69 +121,8 @@ $section_events = array(
 			</section>
 		</section>
 
-		<section class="relative grid grid-cols-1 gap-8 py-8 bg-white">
-			<div class="-z-10 absolute inset-0 bleed bg-[inherit]"></div>
-
-			<header class="text-center">
-				<h2 class="text-4xl font-thin">Stay updated!</h2>
-			</header>
-
-			<section class="grid gap-6">
-				<header>
-					<h3 class="text-xl font-light">Featured talents</h3>
-				</header>
-				<div class="grid rounded-2xl overflow-hidden">
-					<?php set_query_var( 'slider', array( 'slides' => $section_talent['posts'], 'component' => 'template-parts/base/slider-slide-talent' ) ); ?>
-					<?php get_template_part( 'template-parts/base/slider' ); ?>
-				</div>
-				<footer class="text-center">
-					<?php set_query_var( 'button', array( 'label' => 'See all talents' ) ); ?>
-					<?php get_template_part( 'template-parts/base/button' ); ?>
-				</footer>
-			</section>
-
-			<section class="grid gap-6">
-				<header>
-					<h3 class="text-xl font-light">Latest blogposts</h3>
-				</header>
-				<div class="grid grid-cols-1 gap-4">
-					<?php
-						if( $section_blog['posts']->have_posts() ):
-							while( $section_blog['posts']->have_posts() ): $section_blog['posts']->the_post();
-								get_template_part( 'template-parts/base/card-post' );
-							endwhile;
-						endif;
-					?>
-				</div>
-				<footer class="text-center">
-					<?php set_query_var( 'button', array( 'label' => 'See all posts' ) ); ?>
-					<?php get_template_part( 'template-parts/base/button' ); ?>
-				</footer>
-			</section>
-
-			<section class="grid gap-6">
-				<header>
-					<h3 class="text-xl font-light">Upcoming events</h3>
-				</header>
-				<div class="grid grid-cols-2 gap-4">
-					<?php
-						if( $section_events['posts']->have_posts() ):
-							while( $section_events['posts']->have_posts() ): $section_events['posts']->the_post();
-								get_template_part( 'template-parts/base/card-event' );
-							endwhile;
-						endif;
-					?>
-				</div>
-				<footer class="text-center">
-					<?php set_query_var( 'button', array( 'label' => 'See all events' ) ); ?>
-					<?php get_template_part( 'template-parts/base/button' ); ?>
-				</footer>
-			</section>
-		</section>
-
 
 		<?php
-
 		// if ( have_posts() ) {
 		// 	while ( have_posts() ) {
 		// 		the_post();
@@ -192,8 +131,69 @@ $section_events = array(
 		// } else {
 		// 	get_template_part( 'template-parts/post/content', 'none' );
 		// };
-
 		?>
+
+		<?php if ( is_front_page() ) : ?>
+			<section class="relative grid grid-cols-1 gap-8 py-8 bg-white">
+				<div class="-z-10 absolute inset-0 bleed bg-[inherit]"></div>
+
+				<header class="text-center">
+					<h2 class="text-4xl font-thin">Stay updated!</h2>
+				</header>
+
+				<section class="grid gap-6">
+					<header>
+						<h3 class="text-xl font-light">Featured talents</h3>
+					</header>
+					<div class="grid rounded-2xl overflow-hidden">
+						<?php set_query_var( 'slider', array( 'slides' => $section_talent['posts'], 'component' => 'template-parts/base/slider-slide-talent' ) ); ?>
+						<?php get_template_part( 'template-parts/base/slider' ); ?>
+					</div>
+					<footer class="text-center">
+						<?php set_query_var( 'button', array( 'label' => 'See all talents' ) ); ?>
+						<?php get_template_part( 'template-parts/base/button' ); ?>
+					</footer>
+				</section>
+
+				<section class="grid gap-6">
+					<header>
+						<h3 class="text-xl font-light">Latest blogposts</h3>
+					</header>
+					<div class="grid grid-cols-1 gap-4">
+						<?php
+							if( $section_blog['posts']->have_posts() ):
+								while( $section_blog['posts']->have_posts() ): $section_blog['posts']->the_post();
+									get_template_part( 'template-parts/base/card-post' );
+								endwhile;
+							endif;
+						?>
+					</div>
+					<footer class="text-center">
+						<?php set_query_var( 'button', array( 'label' => 'See all posts' ) ); ?>
+						<?php get_template_part( 'template-parts/base/button' ); ?>
+					</footer>
+				</section>
+
+				<section class="grid gap-6">
+					<header>
+						<h3 class="text-xl font-light">Upcoming events</h3>
+					</header>
+					<div class="grid grid-cols-2 gap-4">
+						<?php
+							if( $section_events['posts']->have_posts() ):
+								while( $section_events['posts']->have_posts() ): $section_events['posts']->the_post();
+									get_template_part( 'template-parts/base/card-event' );
+								endwhile;
+							endif;
+						?>
+					</div>
+					<footer class="text-center">
+						<?php set_query_var( 'button', array( 'label' => 'See all events' ) ); ?>
+						<?php get_template_part( 'template-parts/base/button' ); ?>
+					</footer>
+				</section>
+			</section>
+		<?php endif; ?>
 
 	</article>
 
