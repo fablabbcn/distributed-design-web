41a0e4e6378997b0574ef20ed48fa615c7b7d48d
Nestor Vera <nestorvera@me.com>
2023-03-21 17:02:38 +0100

    Fix newsletter section responsive styles

--- template-parts/blocks/footer/newsletter.php
+++ template-parts/blocks/footer/newsletter.php
@@ -10,15 +10,17 @@ $n_classes = array(
 ?>
 
 
-<div class="grid gap-8">
+<div class="container grid-layout gap-y-8 lg:items-baseline">
 
-	<div class="grid grid-cols-[1fr_auto] gap-4 items-end">
-		<!-- <p class="text-4xl leading-none font-thin"><?php echo esc_html( $footer_cta['title'] ); ?></p> -->
-		<p class="text-4xl leading-none font-thin">Join our community!</p>
-		<p class="text-xl leading-tight font-light">Sign up to our newsletter and stay updated on the latest Distributed Design news</p>
+	<div class="col-span-full lg:col-span-3">
+		<div class="grid-layout grid-cols-[1fr_auto] lg:grid-cols-1 items-end lg:pr-16">
+			<!-- <p class="text-4xl leading-none font-thin"><?php echo esc_html( $footer_cta['title'] ); ?></p> -->
+			<p class="text-4xl leading-none font-thin">Join our community!</p>
+			<p class="text-xl leading-tight font-light">Sign up to our newsletter and stay updated on the latest Distributed Design news</p>
+		</div>
 	</div>
 
-	<div class="">
+	<div class="col-span-full lg:col-span-4">
 		<form data-ajaxchimp class="" method="POST"
 			action="//fablabbcn.us2.list-manage.com/subscribe/post?u=d67ba8deb34a23a222ec4eb8a&id=9ece23d947">
 
