8aa825a41fa4e0930ce81c914c5b62fa29642577
Nestor Vera <nestorvera@me.com>
2023-03-23 13:17:37 +0100

    Add custom icons to slider navigation arrows

--- template-parts/base/slider.php
+++ template-parts/base/slider.php
@@ -13,12 +13,20 @@
     <?php if ( array_key_exists( 'pagination', $slider ) ? $slider['pagination'] : true ) : ?>
       <div class="swiper-pagination"></div>
     <?php endif ?>
-    <?php if ( array_key_exists( 'navigation', $slider ) ? $slider['navigation'] : true ) : ?>
-      <div class="swiper-button-prev"></div>
-      <div class="swiper-button-next"></div>
-    <?php endif ?>
     <?php if ( array_key_exists( 'scrollbar', $slider ) ? $slider['scrollbar'] : false ) : ?>
       <div class="swiper-scrollbar"></div>
+    <?php endif ?>
+    <?php if ( array_key_exists( 'navigation', $slider ) ? $slider['navigation'] : true ) : ?>
+      <div class="swiper-button-prev after:!content-none">
+        <svg class="w-8 fill-none stroke-[var(--swiper-theme-color)] stroke-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 39.65 77.12">
+          <path d="M115.72 821.59c0 38.55-38.65 38.55-38.65 38.55s38.65 0 38.65 38.56" transform="translate(-77.07 -821.59)"/>
+        </svg>
+      </div>
+      <div class="swiper-button-next after:!content-none">
+        <svg class="w-8 fill-none stroke-[var(--swiper-theme-color)] stroke-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 39.65 77.12">
+          <path d="M963.88 898.7c0-38.56 38.65-38.56 38.65-38.56s-38.65 0-38.65-38.55" transform="translate(-962.88 -821.59)"/>
+        </svg>
+      </div>
     <?php endif ?>
   <?php endif ?>
 
