f2b68bdbaa7fecc631a05f9a09142e721fe54337
Nestor Vera <nestorvera@me.com>
2023-03-27 15:30:15 +0200

    Fix component not using vector arrow

--- template-parts/base/card-post.php
+++ template-parts/base/card-post.php
@@ -9,7 +9,11 @@
       <p class="col-span-2 -mr-4 text-xl line-clamp-3"><?php the_title(); ?></p>
       <p class="col-span-2 text-sm uppercase"><?php the_date( 'd F Y' ); ?></p>
       <div class="ml-auto">
-        <button class="w-10 h-10 flex justify-center items-center text-center border rounded-full">-></button>
+        <button class="flex justify-center items-center w-10 h-10 p-2 text-center border rounded-full">
+          <svg class="fill-none stroke-current stroke-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 39.48 40.41">
+            <path d="M21.13 39.67c0-19.46 17.6-19.46 17.6-19.46s-17.6 0-17.6-19.46M.75 20.24l37.98-.03" />
+          </svg>
+        </button>
       </div>
     </div>
   </div>
