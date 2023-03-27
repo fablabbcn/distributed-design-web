93220c4ece7ffcbae3de91aa611eaf36aefe9927
Nestor Vera <nestorvera@me.com>
2023-03-27 13:51:39 +0200

    Remove old unused files

--- template-parts/front-page/section-header.php
+++
@@ -1,7 +0,0 @@
-<?php if ( $caption ) : ?>
-
-	<header class="py-20 px-15 lg:px-45 lg:uppercase border-b">
-		<h2 class="-mb-border text-12 lg:text-16 leading-normal font-bold lg:font-normal"><?php echo esc_html( $caption ); ?></h2>
-	</header>
-
-<?php endif ?>
