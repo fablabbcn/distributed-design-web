386e377b1d7e6d1c3c9a554c524ce98d8ede0590
Nestor Vera <nestorvera@me.com>
2023-04-13 17:52:04 +0200

    Fix space between title and sections being too big

--- template-parts/blocks/header.php
+++ template-parts/blocks/header.php
@@ -1,4 +1,4 @@
-<header class="grid grid-cols-5 gap-4 py-4">
+<header class="grid grid-cols-5 gap-4 mt-4">
 
 	<div class="col-start-3 col-span-3 lg:col-start-2 lg:col-span-2">
 		<h1 class="text-2xl leading-tight font-light"><?php echo esc_html( $title ); ?></h1>
