012c05c203ecf0eafac5afa092a2b8d466894b7c
Nestor Vera <nestorvera@me.com>
2023-04-14 16:33:18 +0200

    Fix container not growing

--- template-parts/blocks/acf-flexible-content/layout-banner.php
+++ template-parts/blocks/acf-flexible-content/layout-banner.php
@@ -20,7 +20,7 @@ $className = array(
   <div aria-hidden="true" class="-z-10 absolute inset-y-0 bg-[inherit] w-[50vw] right-[50%]"></div>
 
   <div class="col-span-full lg:col-start-2 lg:col-end-7 flex justify-center items-center py-8 lg:p-16">
-    <div class="flex flex-col text-3xl leading-tight [&_br]:hidden <?php echo esc_attr( $styles['font_weight'] ?: 'font-thin' ); ?>">
+    <div class="flex flex-col w-full text-3xl leading-tight [&_br]:hidden <?php echo esc_attr( $styles['font_weight'] ?: 'font-thin' ); ?>">
       <?php echo wp_kses_ddmp( $description ); ?>
     </div>
   </div>
