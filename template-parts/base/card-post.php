dbce9437bb1b27a61c850fd8ba1c7f1f26459c56
Nestor Vera <nestorvera@me.com>
2023-04-17 18:45:58 +0200

    Fix arrow icon not showing up in Safari

--- template-parts/base/card-post.php
+++ template-parts/base/card-post.php
@@ -10,7 +10,7 @@
       <p class="col-span-2 text-sm uppercase"><?php the_date( 'd F Y' ); ?></p>
       <div class="ml-auto">
         <button class="flex justify-center items-center w-10 h-10 p-2 text-center border rounded-full">
-          <svg class="fill-none stroke-current stroke-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 39.48 40.41">
+          <svg class="w-full h-full fill-none stroke-current stroke-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 39.48 40.41">
             <path d="M21.13 39.67c0-19.46 17.6-19.46 17.6-19.46s-17.6 0-17.6-19.46M.75 20.24l37.98-.03" />
           </svg>
         </button>
