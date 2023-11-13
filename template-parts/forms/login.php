d55e5f96dc416f911dbaa573722beb3389e7273a
Nestor Vera <nestorvera@me.com>
2023-11-13 17:42:16 +0100

    Cleanup form classes

--- template-parts/forms/login.php
+++ template-parts/forms/login.php
@@ -5,17 +5,17 @@ $login_url = wp_lostpassword_url( get_permalink() );
 ?>
 
 
-<fieldset class="w-full mb-20">
+<fieldset class="flex flex-col gap-4 w-full mb-4">
 
-	<p class="mb-20 text-16 md:text-22 font-oswald font-medium md:text-center uppercase">Log In</p>
+	<p class="">Log In</p>
 
-	<input name="username" id="username" class="<?php echo esc_attr( $form_classes['input'] ); ?> -mb-border"
+	<input name="username" id="username" class="<?php echo esc_attr( $form_classes['input'] ); ?>"
 		type="text" placeholder="Username or Email" autocomplete="current-email">
 
-	<input name="password" id="password" class="<?php echo esc_attr( $form_classes['input'] ); ?> -mb-border"
+	<input name="password" id="password" class="<?php echo esc_attr( $form_classes['input'] ); ?>"
 		type="password" placeholder="Password" autocomplete="current-password">
 
-	<p class="mt-10 md:mt-25 text-12 md:text-16 md:text-center">
+	<p class="">
 		<a class="underline" href="<?php echo esc_attr( $login_url ); ?>">Forgot your password?</a>
 	</p>
 
