84dd90a444d0bb3af8a6dae26112b5cc449201b1
usuaria <unicorn@llos.co>
2024-09-17 11:19:08 +0200

    Refactor partner modal into own template_part

--- template-parts/blocks/acf-flexible-content/layout-partners-map.php
+++ template-parts/blocks/acf-flexible-content/layout-partners-map.php
@@ -55,10 +55,8 @@ $partner_types = get_terms( array(
                 <?php foreach( $partners as $partner ) : ?>
                     <?php 
                     $location = get_field('location', $partner->ID);
+                    $partner_type = get_the_terms($partner->ID, 'partner_type') ? get_the_terms($partner->ID, 'partner_type')[0] : null;
                     if ( $location ) : 
-                        $partner_type = get_the_terms($partner->ID, 'partner_type') ? get_the_terms($partner->ID, 'partner_type')[0] : null;
-                        $modal_title = get_field('modal_title', $partner->ID) ? get_field('modal_title', $partner->ID) : get_the_title($partner->ID);
-                        $gallery = get_field('gallery', $partner->ID);
                     ?>
                         <div
                             class="marker opacity-0"
@@ -68,36 +66,14 @@ $partner_types = get_terms( array(
                             data-color="<?php echo esc_attr( get_field('type_color', $partner_type) ); ?>"
                             data-filter="<?php echo esc_attr( $partner_type->slug ); ?>"
                         >
-                            <div class="flex flex-col lg:flex-row gap-4 lg:gap-6 p-1 lg:p-4">
-                                <div class="w-full lg:max-w-[200px]">
-									<div class="text-xl lg:text-3xl"><?php echo wp_kses_ddmp( $modal_title ); ?></div>
-									<?php if ( get_field('link', $partner->ID) ) : ?>
-									<?php $link = get_field('link', $partner->ID); ?>
-										<a target="_blank" class="absolute top-4 left-4 lg:top-auto lg:bottom-4 text-sm leading-none border border-black focus-visible:outline-none focus-visible:border-black px-4 lg:px-6 py-1 lg:py-2 rounded-full no-underline hover:bg-black hover:text-white transition-colors"  href="<?php echo esc_url( $link['url'] ); ?>">Website</a>
-									<?php endif; ?>
-								</div>
-                                <div class="flex flex-col lg:flex-1 gap-y-4 lg:gap-y-6 leading-normal">
-                                    <div class="pb-4 lg:pb-6 border-b text-base">
-										<?php echo wp_kses_ddmp( get_field('description', $partner->ID) ); ?>
-									</div>
-									<?php if ( get_field('modal_what', $partner->ID) ) : ?>
-										<div class="flex flex-col lg:flex-row gap-4 lg:gap-10">
-											<div class="min-w-[210px] max-w-[30%] text-2xl"><?php get_field('modal_what_title', $partner->ID) ? wp_kses_ddmp( get_field('modal_what_title', $partner->ID) ) : 'What do they do?'; ?></div>
-											<div class="text-sm flex flex-col gap-y-2"><?php echo wp_kses_ddmp( get_field('modal_what', $partner->ID) ); ?></div>
-										</div>
-									<?php endif; ?>
-
-                                    <?php if ($gallery) : ?>
-                                        <div class="grid-layout grid-cols-1 lg:grid-cols-12 gap-3">
-                                            <?php foreach ($gallery as $image) : ?>
-                                                <div class="col-span-4 aspect-1">
-                                                    <img class="w-full h-full object-cover rounded-2xl" src="<?php echo esc_url($image['sizes']['large']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" class="w-full h-auto">
-                                                </div>
-                                            <?php endforeach; ?>
-                                        </div>
-                                    <?php endif; ?>
-                                </div>
-                            </div>
+                           <?php get_template_part(
+                                'template-parts/base/partner/modal',
+                                'partner-modal', 
+                                array(
+                                    'partner' => $partner,
+                                    'partner_type' => $partner_type,
+                                ));
+                            ?> 
                         </div>
                     <?php endif; ?>
                 <?php endforeach; ?>
