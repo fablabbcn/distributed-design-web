c447174745b3a0fa9cf15da6aac81018eb0446a9
Sergio Barriga <sergio@llos.co>
2024-09-18 15:08:17 +0200

    popup mobile

--- template-parts/blocks/acf-flexible-content/layout-section-members.php
+++ template-parts/blocks/acf-flexible-content/layout-section-members.php
@@ -32,7 +32,7 @@ $members_section_members = get_sub_field('members');
                 </div>
             </div>
             <div 
-                class="fixed z-50 px-10 py-20 bg-white min-h-screen left-0 top-0 opacity-0 w-full transition-opacity duration-500 ease-in-out pointer-events-none" 
+                class="fixed z-50 px-5 lg:px-10 py-20 bg-white min-h-screen left-0 top-0 opacity-0 w-full transition-opacity duration-500 ease-in-out pointer-events-none" 
                 data-membermap="<?php echo $num_member; ?>"
             >
                 <div class="absolute top-10 right-10 cursor-pointer" data-closemap="<?php echo $num_member; ?>">
@@ -40,7 +40,7 @@ $members_section_members = get_sub_field('members');
                         <path fill="black" d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z"/>
                     </svg>
                 </div>
-                <div class="relative h-[calc(100vh-80px)] overflow-y-auto">
+                <div class="relative h-[calc(100vh-80px)] overflow-y-auto pt-12 lg:pt-0">
                     <?php get_template_part(
                         'template-parts/base/partner/modal',
                         'partner-modal', 
