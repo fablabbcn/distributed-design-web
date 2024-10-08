941cd29cacd05c41ea996848aad5c0c858a6486d
Sergio Barriga <sergio@llos.co>
2024-10-08 17:01:22 +0200

    remove debug tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -51,6 +51,6 @@ module.exports = {
     require('@tailwindcss/typography'),
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
-    require('@vicgutt/tailwindcss-debug'),
+    //require('@vicgutt/tailwindcss-debug'),
   ],
 }
