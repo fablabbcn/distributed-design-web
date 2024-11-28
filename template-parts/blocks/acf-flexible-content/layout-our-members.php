3ac2bbc0dadedb76ea2fe6d6d55a3d5a5adc8336
Sergio Barriga <sergio@llos.co>
2024-11-28 12:57:02 +0100

    new disposition logos

--- template-parts/blocks/acf-flexible-content/layout-our-members.php
+++ template-parts/blocks/acf-flexible-content/layout-our-members.php
@@ -12,8 +12,8 @@ $args_our_members = array(
 $members_our_members = new WP_Query( $args_our_members );
 ?>  
 
-<div class="py-20 px-10 grid grid-cols-12 lg:gap-20 bg-white">
-    <div class="col-span-12 lg:col-span-4 flex flex-col justify-between gap-5">
+<div class="py-20 px-10 grid grid-cols-12 gap-4 lg:gap-20 bg-white">
+    <div class="col-span-12 lg:col-span-4 flex flex-col gap-5">
         <h3 class="text-5xl">
             <?php echo $title_our_members; ?>
         </h3>
@@ -21,12 +21,12 @@ $members_our_members = new WP_Query( $args_our_members );
             <?php echo $text_our_members; ?>
         </div>
     </div>
-    <div class="col-span-12 lg:col-span-8 flex justify-center items-center gap-4 flex-wrap brightness-0 max-w-[1000px] mx-auto">
+    <div class="col-span-12 lg:col-span-8 grid grid-cols-4 lg:grid-cols-6 brightness-0 opacity-80 max-w-[700px] mx-auto">
         <?php if($members_our_members->have_posts()): ?>
             <?php while($members_our_members->have_posts()): $members_our_members->the_post(); ?>
-            <figure class="flex">
-                <a href="<?php echo (get_field('external_link', get_the_ID())) ? (get_field('external_link', get_the_ID())) : the_permalink(); ?>" <?php echo (get_field('external_link', get_the_ID())) ? 'target="_blank"' : '' ?> >
-                    <img class="w-auto max-h-8" src="<?php echo get_field('logo', get_the_ID())['url'] ?>" alt="<?php echo get_field('logo', get_the_ID())['alt'] ?>">
+            <figure class="flex justify-center w-full aspect-[1.5/1] p-4 lg:p-3">
+                <a class="flex justify-center items-center" href="<?php echo (get_field('external_link', get_the_ID())) ? (get_field('external_link', get_the_ID())) : the_permalink(); ?>" <?php echo (get_field('external_link', get_the_ID())) ? 'target="_blank"' : '' ?> >
+                  <img class="w-full max-h-full object-contain object-center" src="<?php echo get_field('logo', get_the_ID())['url'] ?>" alt="<?php echo get_field('logo', get_the_ID())['alt'] ?>">
                 </a>
             </figure>
             <?php endwhile; ?>
